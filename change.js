ad7e7741ff3dd14eaaaed1ce7d75916d671b7388
pcook3 <philipordrew@gmail.com>
2024-04-17 17:02:18 -0400

    Implemented a more interactive Home Page feature

--- change.js
+++ change.js
@@ -2,3 +2,11 @@ function changeImage(){
     var img = document.getElementById('image');
     img.src = 'IMG_5628.jpg';
 }
+function changeImage1(){
+    var img1 = document.getElementById('image');
+    img1.src = 'IMG_5627.JPG';
+}
+function changeImage2(){
+    var img2 = document.getElementById('image');
+    img2.src = 'IMG_6000.JPG';
+}
